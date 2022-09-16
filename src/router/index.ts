d1fb7674af2dbfe96086baa118abffe359b44f82
staniboy <stanley.v@outlook.com>
2022-09-17 03:41:50 +0400

    feat: routes for Inbox and Today

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,25 @@
-import ProjectView from '@/views/ProjectView.vue';
+import GroupView from '@/views/GroupView.vue';
+import InboxView from '@/views/InboxView.vue';
+import TodayView from '@/views/TodayView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/project/:id',
-            name: 'project',
-            component: ProjectView,
+            path: '/',
+            name: 'inbox',
+            component: InboxView,
+        },
+        {
+            path: '/today',
+            name: 'today',
+            component: TodayView,
+        },
+        {
+            path: '/group/:id',
+            name: 'group',
+            component: GroupView,
         },
     ],
 });
---
+++ src/views/InboxView.vue
@@ -0,0 +1,14 @@
+<template>
+    <DisplayTasks :tasks="tasks" />
+</template>
+
+<script setup lang="ts">
+import DisplayTasks from '@/components/shared/DisplayTasks.vue';
+import { useAppStore } from '@/stores/appStore';
+import { computed } from 'vue';
+
+const store = useAppStore();
+const tasks = computed(() => store.getInboxTasks());
+</script>
+
+<style scoped></style>
