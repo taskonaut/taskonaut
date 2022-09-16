023779994ebfb780cf16d49e95785db7dd4f6b2f
staniboy <stanley.v@outlook.com>
2022-09-16 07:07:12 +0400

    refactor: moved layout to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <template>
     <v-app>
         <v-main>
-            <RouterView />
+            <v-layout>
+                <v-app-bar color="primary" app>
+                    <v-app-bar-nav-icon
+                        variant="text"
+                        @click="toggleDrawer()"
+                    ></v-app-bar-nav-icon>
+                    <v-toolbar-title>Taskominator</v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn variant="text" icon="mdi-magnify"></v-btn>
+                    <v-btn variant="text" icon="mdi-filter"></v-btn>
+                    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+                </v-app-bar>
+
+                <AppSidebar v-model:drawer="drawer" />
+
+                <v-main style="height: 100vh"> </v-main>
+            </v-layout>
         </v-main>
     </v-app>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref(true);
+
+function toggleDrawer() {
+    drawer.value = !drawer.value;
+}
+</script>
+
+<style scoped></style>
