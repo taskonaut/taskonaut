4830811721f6d8b926d32cdb1ad12f4d690fc38c
staniboy <stanley.v@outlook.com>
2022-09-16 08:23:40 +0400

    refactor: removed routes for now

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-    ],
+    routes: [],
 });
 
 export default router;
