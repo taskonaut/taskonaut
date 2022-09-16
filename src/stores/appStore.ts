cf8841352e698f05bff0f1ff6532c8103dda3f12
staniboy <stanley.v@outlook.com>
2022-09-16 08:42:56 +0400

    refactor: model and store experiments

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -4,47 +4,54 @@ import { defineStore } from 'pinia';
 export const useAppStore = defineStore({
     id: 'appStore',
     state: () => ({
+        inbox: {
+            taskOrder: [] as string[],
+            tasks: [] as Task[],
+        },
         lists: [
             {
                 uuid: '1',
-                name: 'various things',
-                taskIds: ['1', '2'],
+                name: 'Keep On Livin 🌭',
+                taskOrder: [],
+                tasks: [
+                    {
+                        uuid: '1',
+                        header: 'Breathe',
+                        body: 'this have no due date',
+                        dateCreated: new Date(),
+                        dateCompleted: null,
+                        dueDate: null,
+                    },
+                    {
+                        uuid: '2',
+                        header: 'Sleep',
+                        body: 'this is due on 29 of September',
+                        dateCreated: new Date(),
+                        dateCompleted: null,
+                        dueDate: new Date(2022, 9, 29),
+                    },
+                    {
+                        uuid: '3',
+                        header: 'Wake Up',
+                        body: 'this is due today',
+                        dateCreated: new Date(),
+                        dateCompleted: null,
+                        dueDate: new Date(),
+                    },
+                ],
             },
             {
                 uuid: '2',
-                name: 'cool things',
-                taskIds: ['1', '2'],
+                name: 'Empty List',
+                taskOrder: [],
+                tasks: [],
             },
         ] as List[],
-        tasks: [
-            {
-                uuid: '1',
-                header: 'this is sample task header',
-                body: 'this have no due date',
-                dateCreated: new Date(),
-                dateCompleted: null,
-                dueDate: null,
-            },
-            {
-                uuid: '2',
-                header: 'this is sample task header 2',
-                body: 'this is due on 29 of September',
-                dateCreated: new Date(),
-                dateCompleted: null,
-                dueDate: new Date(2022, 9, 29),
-            },
-            {
-                uuid: '3',
-                header: 'this is sample task header 2',
-                body: 'this is due today',
-                dateCreated: new Date(),
-                dateCompleted: null,
-                dueDate: new Date(),
-            },
-        ] as Task[],
     }),
     getters: {
         getLists: (state) => state.lists,
+        getListById: (state) => (listId: string) =>
+            state.lists.find((list) => list.uuid == listId),
     },
     actions: {},
 });
