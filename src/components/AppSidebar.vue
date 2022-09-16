d5cccb585925b142517e786518d7a10683429206
staniboy <stanley.v@outlook.com>
2022-09-16 08:23:10 +0400

    feat: added lists from store to nav

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -10,14 +10,26 @@
         </template>
 
         <v-divider></v-divider>
-
         <v-list density="compact" nav>
             <v-list-item
-                v-for="item in items"
+                v-for="item in menuItems"
                 :key="item.title"
                 :prependIcon="item.icon"
                 :title="item.title"
                 :value="item.value"
+                :active="false"
+            >
+            </v-list-item>
+        </v-list>
+        <v-list density="compact" nav select-strategy="single-leaf">
+            <v-list-subheader>PROJECTS</v-list-subheader>
+            <v-list-item
+                v-for="list in lists"
+                :key="list.uuid"
+                :prependIcon="'mdi-circle-small'"
+                :title="list.name"
+                :value="list.name"
+                :active="false"
             >
             </v-list-item>
         </v-list>
@@ -25,14 +37,20 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useAppStore } from '../stores/appStore';
+
 export interface Props {
     drawer: boolean;
 }
+
 const props = withDefaults(defineProps<Props>(), {
     drawer: true,
 });
 
-const items = [
+const store = useAppStore();
+const lists = computed(() => store.getLists);
+const menuItems = [
     { title: 'Inbox', icon: 'mdi-home-city', value: 'inbox' },
     { title: 'Today', icon: 'mdi-account', value: 'today' },
     { title: 'Upcoming', icon: 'mdi-account-group-outline', value: 'upcoming' },
