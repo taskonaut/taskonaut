398c63f390185e9e4f27ea5ed3d1cb506e8823f1
staniboy <stanley.v@outlook.com>
2022-09-17 15:37:35 +0400

    feat: added add button

--- src/views/GroupView.vue
+++ src/views/GroupView.vue
@@ -1,9 +1,12 @@
 <template>
-    <DisplayTasks :tasks="tasks" />
+    <DisplayTasks :tasks="tasks">
+        <template v-slot:list1-slot><AddListItem></AddListItem></template>
+    </DisplayTasks>
 </template>
 
 <script setup lang="ts">
 import DisplayTasks from '@/components/shared/DisplayTasks.vue';
+import AddListItem from '../components/partials/AddListItem.vue';
 import { useAppStore } from '@/stores/appStore';
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
