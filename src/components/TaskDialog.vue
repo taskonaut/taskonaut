d0a089da8eb58fbed428d47b2ea2dc62c73cafde
staniboy <stanley.v@outlook.com>
2022-09-17 19:39:24 +0400

    feat: TaskDialog for AddListItem

---
+++ src/components/TaskDialog.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-dialog
+        :model-value="props.open"
+        activator="parent"
+        fullscreen
+        :scrim="false"
+        transition="dialog-bottom-transition"
+    >
+        <v-card>
+            <v-toolbar dark color="primary">
+                <v-btn icon dark @click="emitClose()">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+                <v-toolbar-title>Settings</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                    <v-btn dark text @click="emitClose()"> Save </v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    open: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'close'): void;
+}>();
+
+function emitClose() {
+    emit('close');
+}
+</script>
+<style scoped></style>
--- src/components/partials/AddListItem.vue
+++ src/components/partials/AddListItem.vue
@@ -1,13 +1,33 @@
 <template>
-    <v-list-item title="Add New Task" value="add" :active="false">
+    <v-list-item
+        title="Add New Task"
+        value="add"
+        :active="false"
+        @click="dialogState = true"
+    >
         <template v-slot:prepend>
             <v-list-item-action start>
-                <v-btn variant="tonal" icon="mdi-plus" size="small"></v-btn>
+                <v-btn
+                    variant="tonal"
+                    icon="mdi-plus"
+                    size="small"
+                    @click="dialogState = true"
+                >
+                </v-btn>
             </v-list-item-action>
+            <TaskDialog
+                :open="dialogState"
+                @close="dialogState = false"
+            ></TaskDialog>
         </template>
     </v-list-item>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import TaskDialog from '../TaskDialog.vue';
+
+const dialogState = ref(false);
+</script>
 
 <style scoped></style>
