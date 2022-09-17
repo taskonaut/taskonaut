e256c84c1e6d5473285c32aecf2367d3bd08a118
4gray <serega05@gmail.com>
2022-09-17 14:14:06 +0200

    refactor: use before each function in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,6 @@ import InboxView from '@/views/InboxView.vue';
 import TodayView from '@/views/TodayView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-const requireAuth = async (to, from, next) => {
-    await useUserStore().currentUser();
-    next();
-};
-
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -16,21 +11,23 @@ const router = createRouter({
             path: '/',
             name: 'inbox',
             component: InboxView,
-            beforeEnter: requireAuth,
         },
         {
             path: '/today',
             name: 'today',
             component: TodayView,
-            beforeEnter: requireAuth,
         },
         {
             path: '/group/:id',
             name: 'group',
             component: GroupView,
-            beforeEnter: requireAuth,
         },
     ],
+});
+
+router.beforeEach(async (to, from, next) => {
+    await useUserStore().currentUser();
+    next();
 });
 
 export default router;
