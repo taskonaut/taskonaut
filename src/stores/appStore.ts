cfdcd05d72ef2bc9f47d9d3bd817c5f677ca9af1
4gray <serega05@gmail.com>
2022-09-22 17:42:16 +0200

    feat: sync tasks from state with localStorage

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -1,62 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
 import type { Group, Task } from '@/model';
 import { defineStore } from 'pinia';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface AppStore {
+    tasks: Task[];
+    groups: Group[];
+}
 
 export const useAppStore = defineStore({
     id: 'appStore',
-    state: () => ({
-        tasks: [
-            {
-                uuid: '1',
-                projectId: '1',
-                header: 'Breathe',
-                body: 'this have no due date',
-                dateCreated: new Date(),
-                complete: true,
-                dateCompleted: null,
-                dueDate: null,
-            },
-            {
-                uuid: '2',
-                projectId: '1',
-                header: 'Sleep',
-                body: 'this is due on 29 of September',
-                dateCreated: new Date(),
-                complete: false,
-                dateCompleted: null,
-                dueDate: new Date(2022, 9, 27),
-            },
-            {
-                uuid: '3',
-                projectId: '1',
-                header: 'Wake Up',
-                body: 'this is due today',
-                dateCreated: new Date(),
-                complete: false,
-                dateCompleted: null,
-                dueDate: new Date(),
-            },
-            {
-                uuid: '4',
-                projectId: null,
-                header: 'Inbox Task',
-                body: 'should be in inbox and today',
-                dateCreated: new Date(),
-                complete: false,
-                dateCompleted: null,
-                dueDate: new Date(),
-            },
-            {
-                uuid: '5',
-                projectId: '3',
-                header: 'Task',
-                body: 'should be in 3rd group and today',
-                dateCreated: new Date(),
-                complete: false,
-                dateCompleted: null,
-                dueDate: new Date(),
-            },
-        ] as Task[],
+    state: (): AppStore => ({
+        tasks: [],
         groups: [
             {
                 uuid: '1',
@@ -73,7 +27,7 @@ export const useAppStore = defineStore({
                 name: '3rd Group',
                 taskOrder: [],
             },
-        ] as Group[],
+        ],
     }),
     getters: {
         getGroups: (state) => state.groups,
@@ -115,6 +69,23 @@ export const useAppStore = defineStore({
             this.tasks.map((task) => {
                 if (task.uuid == taskId) task.complete = !task.complete;
             });
+        },
+    },
+    persist: {
+        storage: localStorage,
+        paths: ['tasks', 'groups'],
+        afterRestore: (ctx) => {
+            // recreate Date objects from stringified json
+            ctx.store.$state.tasks = ctx.store.$state.tasks.map(
+                (task: Task) => ({
+                    ...task,
+                    dateCreated: new Date(task.dateCreated),
+                    dueDate: task.dueDate ? new Date(task.dueDate) : null,
+                    dateCompleted: task.dateCompleted
+                        ? new Date(task.dateCompleted)
+                        : null,
+                })
+            );
         },
     },
 });
