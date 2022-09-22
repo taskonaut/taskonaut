598a0ce503089b53a7b34b913957f754f47b0cb1
staniboy <stanley.v@outlook.com>
2022-09-22 06:45:46 +0400

    feat: basic dialog for add/edit Tasks

--- src/components/TaskDialog.vue
+++ src/components/TaskDialog.vue
@@ -1,37 +1,101 @@
 <template>
     <v-dialog
-        :model-value="props.open"
+        md:fullscreen
+        v-model="dialogOpen"
         activator="parent"
-        fullscreen
         :scrim="false"
         transition="dialog-bottom-transition"
     >
-        <v-card>
+        <v-card min-width="600">
             <v-toolbar dark color="primary">
-                <v-btn icon dark @click="emitClose()">
+                <v-btn icon dark @click="closeDialog()">
                     <v-icon>mdi-close</v-icon>
                 </v-btn>
-                <v-toolbar-title>Settings</v-toolbar-title>
+                <v-toolbar-title v-if="!props.task">
+                    Add New Task
+                </v-toolbar-title>
+                <v-toolbar-title v-if="props.task"> Edit Task </v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-toolbar-items>
-                    <v-btn dark text @click="emitClose()"> Save </v-btn>
-                </v-toolbar-items>
             </v-toolbar>
+            <v-card-text>
+                <v-form
+                    ref="form"
+                    v-model="formData.valid"
+                    :submit="formSubmit"
+                >
+                    <v-text-field
+                        autofocus
+                        v-model="formData.name"
+                        label="Task Name"
+                        :rules="formData.rules"
+                        required
+                        ref="nameInput"
+                    ></v-text-field>
+                    <v-textarea
+                        auto-grow
+                        label="Description (optional)"
+                        v-model="formData.body"
+                        rows="1"
+                        @keydown="textareaHandler($event)"
+                    ></v-textarea>
+                    <v-btn
+                        :disabled="!formData.valid"
+                        color="success"
+                        type="submit"
+                        v-if="!props.task"
+                        >Add</v-btn
+                    >
+                    <v-btn color="success" type="submit" v-if="props.task"
+                        >Save</v-btn
+                    >
+                </v-form>
+            </v-card-text>
         </v-card>
     </v-dialog>
 </template>
 
 <script setup lang="ts">
+import type { Task } from '@/model';
+import { useAppStore } from '@/stores/appStore';
+import { reactive, ref } from 'vue';
+
 const props = defineProps<{
-    open: boolean;
+    task?: Task;
 }>();
 
-const emit = defineEmits<{
-    (e: 'close'): void;
-}>();
+const store = useAppStore();
 
-function emitClose() {
-    emit('close');
+const dialogOpen = ref(false);
+const form = ref();
+const nameInput = ref();
+const formData = reactive({
+    valid: false,
+    rules: [(v: any) => !!v || 'Name is required'],
+    name: props.task?.header || '',
+    body: props.task?.body || '',
+});
+
+function formSubmit() {
+    if (props.task) {
+        store.updateTask(props.task.uuid, formData.name, formData.body);
+        dialogOpen.value = false;
+    } else {
+        store.createTask(formData.name, formData.body);
+        form.value.reset();
+        nameInput.value.focus();
+    }
+}
+function closeDialog() {
+    dialogOpen.value = false;
+    if (!props.task) form.value.reset();
+}
+function textareaHandler(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'Enter') {
+        formData.body += '\n';
+    } else if (!event.ctrlKey && event.key === 'Enter') {
+        event.preventDefault();
+        formSubmit();
+    }
 }
 </script>
 <style scoped></style>
--- src/components/partials/AddListItem.vue
+++ src/components/partials/AddListItem.vue
@@ -1,33 +1,16 @@
 <template>
-    <v-list-item
-        title="Add New Task"
-        value="add"
-        :active="false"
-        @click="dialogState = true"
-    >
+    <v-list-item title="Add New Task" value="add" :active="false">
         <template v-slot:prepend>
             <v-list-item-action start>
-                <v-btn
-                    variant="tonal"
-                    icon="mdi-plus"
-                    size="small"
-                    @click="dialogState = true"
-                >
-                </v-btn>
+                <v-btn variant="tonal" icon="mdi-plus" size="small"></v-btn>
             </v-list-item-action>
-            <TaskDialog
-                :open="dialogState"
-                @close="dialogState = false"
-            ></TaskDialog>
+            <TaskDialog />
         </template>
     </v-list-item>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import TaskDialog from '../TaskDialog.vue';
-
-const dialogState = ref(false);
 </script>
 
 <style scoped></style>
