f5388a3b3caa4fb79ce5aaae26e12064cbd4f69f
staniboy <stanley.v@outlook.com>
2022-10-02 08:35:28 +0400

    feat: custom dark theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,15 +3,25 @@ import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 
 // Vuetify
-import { createVuetify } from 'vuetify';
+import { createVuetify, type ThemeDefinition } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+
+const customDarkTheme: ThemeDefinition = {
+    dark: true,
+    colors: {
+        primary: '#ff5c5c',
+    },
+};
 
 export default createVuetify({
     components,
     directives,
     theme: {
-        defaultTheme: 'dark',
+        defaultTheme: 'customDarkTheme',
+        themes: {
+            customDarkTheme,
+        },
     },
 });
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
