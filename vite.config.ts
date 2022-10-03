40d8c6c58430d04fb5a477725e77b39e1b978fee
4gray <serega05@gmail.com>
2022-10-03 20:32:46 +0200

    refactor: update pwa options

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
@@ -12,8 +12,13 @@ export default defineConfig({
         vue(),
         vueJsx(),
         VitePWA({
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+            registerType: 'autoUpdate',
             manifest: {
                 name: 'Taskominator Dev',
+                description: 'The most awesome todo application',
                 short_name: 'Taskominator',
                 theme_color: '#000000',
                 icons: [
@@ -21,6 +26,7 @@ export default defineConfig({
                         src: 'icon.png',
                         sizes: '200x200',
                         type: 'image/png',
+                        purpose: 'any maskable',
                     },
                 ],
             },
