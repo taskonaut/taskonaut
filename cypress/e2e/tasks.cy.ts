68f491f01399a8a5e491f5ff40a0df6260cec721
4gray <serega05@gmail.com>
2022-10-07 00:04:44 +0200

    test: add more e2e test for tasks handling

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -8,14 +8,75 @@ afterEach(() => {
     }
 });
 
-describe('Test task creation', () => {
-    it('create new task', () => {
-        const taskText = 'Buy milk';
+const createTask = (taskText: string) => {
+    cy.contains('.v-list-item', 'Add New Task').click();
+    cy.get('input').eq(0).type(taskText);
+    cy.get('.v-toolbar-items > .v-btn').click();
+    cy.get('.mdi-close').click();
+};
+
+describe('Test task creation/editing/deletion', () => {
+    const taskText = 'Buy milk';
+    beforeEach(() => {
         cy.visit('/');
-        cy.contains('.v-list-item', 'Add New Task').click();
-        cy.get('input').eq(0).type(taskText);
-        cy.get('.v-toolbar-items > .v-btn').click();
-        cy.get('.mdi-close').click();
+    });
+
+    it('should create new task', () => {
+        createTask(taskText);
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 2);
         cy.contains(taskText).should('exist');
+        cy.get('.completed > .v-list').should('not.exist');
+    });
+
+    it('should set task as done', () => {
+        cy.get('.completed > .v-list').should('not.exist');
+        createTask(taskText);
+        cy.get('.v-selection-control__input').find('input').click();
+        cy.get('.completed > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 1);
+    });
+
+    it('should delete task', () => {
+        createTask(taskText);
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 2);
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .eq(0)
+            .trigger('mouseover');
+        cy.get('.show-on-hover').eq(1).invoke('show').click();
+        cy.contains('Delete Task?').should('exist');
+        cy.get('.v-card-actions > .v-btn').click();
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 1);
+        cy.get('.completed > .v-list').should('not.exist');
+        cy.contains(taskText).should('not.exist');
+    });
+
+    it('should edit task', () => {
+        const updatedText = 'Buy juice';
+        createTask(taskText);
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 2);
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .eq(0)
+            .trigger('mouseover');
+        cy.get('.show-on-hover').eq(0).invoke('show').click();
+        cy.contains('Edit Task').should('exist');
+        cy.get('input').eq(1).clear().type(updatedText);
+        cy.get('.v-toolbar-items > .v-btn').click();
+        cy.get('.v-container > .v-list')
+            .find('.v-list-item')
+            .should('have.length', 2);
+        cy.get('.completed > .v-list').should('not.exist');
+        cy.contains(taskText).should('not.exist');
+        cy.contains(updatedText).should('exist');
     });
 });
