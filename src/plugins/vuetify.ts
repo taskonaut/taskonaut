29ec4c9737c73a0e1ec6d36159d71ae5a76c8484
staniboy <stanley.v@outlook.com>
2022-10-08 19:43:24 +0400

    feat: specify icon set

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,7 @@ import 'vuetify/styles';
 
 // Vuetify
 import { createVuetify, type ThemeDefinition } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
@@ -30,6 +31,13 @@ export default createVuetify({
         themes: {
             customDarkTheme,
             customLightTheme,
+        },
+    },
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
         },
     },
 });
