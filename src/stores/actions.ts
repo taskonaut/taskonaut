8f5a77719397bb468b984649333f247b24b4d708
4gray <serega05@gmail.com>
2022-10-14 20:16:48 +0200

    refactor: define actions

---
+++ src/stores/actions.ts
@@ -0,0 +1,15 @@
+export const CREATE_TASK = 'createTask';
+export const DELETE_TASK = 'deleteTask';
+export const TOGGLE_TASK = 'toggleTask';
+export const UPDATE_TASK = 'updateTask';
+
+export const ADD_TO_TASK_ORDER = 'addToTaskOrder';
+export const DELETE_FROM_TASK_ORDER = 'deleteFromTaskOrder';
+
+export const CREATE_GROUP = 'createGroup';
+export const DELETE_GROUP = 'deleteGroup';
+export const RESET_GROUP = 'resetGroup';
+export const UPDATE_GROUP = 'updateGroup';
+
+export const SET_GROUP_ORDER = 'setGroupOrder';
+export const SET_TASK_ORDER = 'setTaskOrder';
