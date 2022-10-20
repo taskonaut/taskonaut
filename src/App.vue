70844dc3ce477f74389d106e2ee178162fd5bb25
4gray <serega05@gmail.com>
2022-10-20 22:19:16 +0200

    feat: add bottom navigation panel

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
             ></v-btn>
         </v-app-bar>
         <AppSidebar v-model="drawer" />
-        <v-main :scrollable="true">
+        <v-main :scrollable="true" :class="smAndDown && 'pb-16'">
             <v-progress-linear
                 v-if="useUserStore().isLoading"
                 indeterminate
@@ -32,9 +32,21 @@
                 class="pa-0"
                 :class="lgAndUp && 'w-75'"
             >
-                <router-view :key="useRoute().fullPath"
-            /></v-container>
+                <router-view :key="useRoute().fullPath" />
+            </v-container>
         </v-main>
+        <SwipeBottomNavigation
+            v-if="smAndDown"
+            swiper-color="orange"
+            background-color="#212121"
+            icon-color="orange"
+            :options="options"
+            v-model="selected"
+        >
+            <template #icon="{ props }">
+                <v-icon>{{ props.icon }}</v-icon>
+            </template>
+        </SwipeBottomNavigation>
         <TaskDialog v-if="showDialog" v-model="showDialog" />
     </v-app>
     <ReloadPrompt />
@@ -50,12 +62,38 @@ import AppSidebar from '@/components/AppSidebar/AppSidebar.vue';
 import TaskDialog from '@/components/dialogs/TaskDialog.vue';
 import ReloadPrompt from '@/components/ReloadPrompt.vue';
 import router from './router';
+import { SwipeBottomNavigation } from 'bottom-navigation-vue';
+import 'bottom-navigation-vue/dist/style.css';
 
-const { lgAndUp } = useDisplay();
+const { lgAndUp, smAndDown } = useDisplay();
 const showDialog = ref(false);
 const drawer = ref(true);
 const appStore = useAppStore();
 const theme = useTheme();
+
+const selected = ref(1); // TODO: should depend on actual route
+const options = [
+    {
+        id: 1,
+        icon: 'mdi-inbox-arrow-down',
+        title: 'Inbox',
+        path: { name: 'inbox' },
+    },
+    {
+        id: 2,
+        icon: 'mdi-calendar-clock',
+        title: 'Today',
+        path: { name: 'today' },
+    },
+    {
+        id: 3,
+        icon: 'mdi-view-week',
+        title: 'Upcoming',
+        path: { name: 'upcoming' },
+    },
+    { id: 4, icon: 'mdi-clock', title: 'Expired', path: { name: 'expired' } },
+    { id: 5, icon: 'mdi-cog', title: 'Settings', path: { name: 'settings' } },
+];
 
 onMounted(async () => {
     await useUserStore().getAuthState();
@@ -82,6 +120,13 @@ function getRouteName(): string {
 </script>
 
 <style scoped>
+.sm-btn-container-foreground {
+    box-shadow: 0px 2px 4px -1px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)),
+        0px 4px 5px 0px
+            var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)),
+        0px 1px 10px 0px
+            var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12));
+}
 #routeName {
     text-transform: capitalize;
 }
