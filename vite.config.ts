6b241a1638e8c34944417fe04aceff18cbf99ad5
staniboy <stanley.v@outlook.com>
2022-11-04 01:08:55 +0400

    feat: vite localhost https

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 import removeConsole from 'vite-plugin-remove-console';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
+    server: { https: true },
     plugins: [
+        mkcert(),
         vue(),
         vueJsx(),
         VitePWA({
