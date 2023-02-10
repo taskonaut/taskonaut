622aa5e33674dbddd7152aba668aba942fe116df
staniboy <stanley.v@outlook.com>
2023-02-10 15:20:01 +0400

    refactor: groupId -> parentId, removed subTasks[]

--- src/model.ts
+++ src/model.ts
@@ -1,14 +1,13 @@
 export interface Task {
     uuid: string;
+    parentId: undefined | string;
     createdBy: string;
-    groupId: undefined | string;
     header: string;
     body: undefined | string;
     dateCreated: number;
     dueDate: undefined | number;
     complete: boolean;
     dateCompleted: undefined | null | number;
-    subTasks?: Task[];
 }
 
 export interface Group {
