f055a769744c65ae38d810635a9fdc1da410df5f
staniboy <stanley.v@outlook.com>
2023-11-07 19:55:58 +0400

    refactor: use snapshot vs store

--- src/views/GroupView.vue
+++ src/views/GroupView.vue
@@ -1,17 +1,37 @@
 <template>
-    <DisplayTasks :tasks="tasks" :meta-id="groupId" />
+    <TaskList v-if="group" v-model="group!.tasks" :top-level="true" />
 </template>
 
 <script setup lang="ts">
 import { useAppStore } from '@/stores/appStore';
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import DisplayTasks from '@/components/shared/DisplayTasks.vue';
+import { onSnapshot, doc } from '@firebase/firestore';
+import { db } from '@/includes/firebase';
+import TaskList from '@/components/shared/TaskList.vue';
+import router from '@/router';
+import { computed, onUnmounted, ref, watch } from 'vue';
+import type { Group } from '@/model';
+import { onMounted } from 'vue';
 
-const store = useAppStore();
+const groupId = computed(() => router.currentRoute.value.params.id as string);
 
-const groupId = computed(() => useRoute().params.id as string);
-const tasks = computed(() => store.getGroupTasks(groupId.value));
+const group = ref();
+let unsubscribe: Function;
+
+onMounted(() => {
+    unsubscribe = onSnapshot(doc(db, 'groups', groupId.value), (doc) => {
+        group.value = doc.data() as unknown as Group;
+    });
+});
+
+onUnmounted(() => {
+    unsubscribe();
+});
+
+watch(
+    group,
+    (group) => {
+        useAppStore().updateGroup(group);
+    },
+    { deep: true }
+);
 </script>
-
-<style scoped></style>
