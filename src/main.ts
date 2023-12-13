40b0cb72f69966af5ef95fd26de8a5f0a55345cb
staniboy <stanley.v@outlook.com>
2023-12-13 09:31:51 +0400

    refactor: remove unused components

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,11 @@ import './assets/main.css';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import router from './router';
-
-import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-
-import { VueFire, VueFireAuth } from 'vuefire';
-import { app as firebaseApp } from './includes/firebase';
 
 const pinia = createPinia();
 const app = createApp(App);
-// eslint-disable-next-line vue/multi-word-component-names
-app.component('Datepicker', Datepicker);
 
 loadFonts();
-
 app.use(pinia).use(router).use(vuetify);
-app.use(VueFire, {
-    firebaseApp,
-    modules: [VueFireAuth()],
-});
 app.mount('#app');
