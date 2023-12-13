382ae6e5391778bebd72140bf4c9956b35635044
staniboy <stanley.v@outlook.com>
2023-12-13 09:44:39 +0400

    feat: add updateTask, deleteTask

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import type { Group, Task } from '@/model';
 import { ref } from 'vue';
-import { db, auth } from '@/includes/firebase';
+import { usersRef, groupsRef, auth } from '@/includes/firebase';
 import {
     doc,
     getDoc,
@@ -9,27 +9,28 @@ import {
     setDoc,
     deleteDoc,
     query,
-    collection,
     where,
     onSnapshot,
 } from '@firebase/firestore';
-
-const USERS = collection(db, 'users');
-const GROUPS = collection(db, 'groups');
+import {
+    filterTaskArray,
+    resetTaskArray,
+    updateArrayTask,
+} from './utils.service';
 
 // User methods
 
 export async function getInbox() {
-    const userSnapshot = await getDoc(doc(USERS, auth.currentUser?.uid));
+    const usersRefnapshot = await getDoc(doc(usersRef, auth.currentUser?.uid));
 
-    const inbox = ref(userSnapshot.data()!.inbox || []);
-    return { inbox };
+    const inbox: Task[] = usersRefnapshot.data()!.inbox || [];
+    return inbox;
 }
 
 export function onInbox() {
     const inbox = ref<Task[]>([]);
     const unsubscribe = onSnapshot(
-        doc(USERS, auth.currentUser?.uid),
+        doc(usersRef, auth.currentUser?.uid),
         (user) => {
             inbox.value = (user.data()!.inbox as Task[]) || [];
         }
@@ -38,7 +39,7 @@ export function onInbox() {
 }
 
 export async function updateInbox(tasks: Task[]) {
-    return await updateDoc(doc(USERS, auth.currentUser?.uid), {
+    return await updateDoc(doc(usersRef, auth.currentUser?.uid), {
         inbox: [...tasks],
     });
 }
@@ -46,24 +47,24 @@ export async function updateInbox(tasks: Task[]) {
 // Group Lists methods
 
 export async function getGroupList() {
-    const userSnapshot = await getDoc(doc(USERS, auth.currentUser?.uid));
-    const groupList = ref(userSnapshot.data()!.groupList || []);
+    const usersRefnapshot = await getDoc(doc(usersRef, auth.currentUser?.uid));
+    const groupList = ref<Group[]>(usersRefnapshot.data()!.groupList || []);
     return { groupList };
 }
 
 export function onGroupList() {
     const list = ref<any[]>([]);
     const unsubscribe = onSnapshot(
-        doc(USERS, auth.currentUser?.uid),
+        doc(usersRef, auth.currentUser?.uid),
         (user) => {
-            list.value = user.data()!.groupList || [];
+            list.value = user.data()!.groupList;
         }
     );
     return { groupList: list, unsubscribe };
 }
 
 export async function updateGroupList(list: any[]) {
-    return await updateDoc(doc(USERS, auth.currentUser?.uid), {
+    return await updateDoc(doc(usersRef, auth.currentUser?.uid), {
         groupList: [...list],
     });
 }
@@ -73,7 +74,7 @@ export async function updateGroupList(list: any[]) {
 export async function getGroups() {
     const groups = ref<Group[]>([]);
     const groupsQuery = query(
-        GROUPS,
+        groupsRef,
         where('ownerId', '==', auth.currentUser?.uid)
     );
     const groupsSnapshot = await getDocs(groupsQuery);
@@ -83,48 +84,68 @@ export async function getGroups() {
     return { groups };
 }
 
-export function onGroups() {
-    const groups = ref<Group[]>([]);
-    const groupsQuery = query(
-        GROUPS,
-        where('ownerId', '==', auth.currentUser?.uid)
-    );
-    const unsubscribe = onSnapshot(groupsQuery, (snapshot) => {
-        groups.value = [];
-        snapshot.docs.forEach((doc) =>
-            groups.value.push(doc.data() as unknown as Group)
-        );
-    });
-    return { groups, unsubscribe };
-}
-
 // Group methods
 
-export async function getGroup(id: string) {
-    const snapshot = await getDoc(doc(GROUPS, id));
-    const group = snapshot.data();
-    return { group };
+export async function getGroup(id: string): Promise<Group> {
+    const snapshot = await getDoc(doc(groupsRef, id));
+    const group = snapshot.data() as Group;
+    return group;
 }
 
 export function onGroup(id: string) {
     const group = ref<Group>();
-    const unsubscribe = onSnapshot(doc(GROUPS, id), async (doc) => {
+    const unsubscribe = onSnapshot(doc(groupsRef, id), async (doc) => {
         group.value = doc.data() as unknown as Group;
     });
     return { group, unsubscribe };
 }
 
 export async function updateGroup(group: Partial<Group>) {
-    const docRef = doc(GROUPS, group.uuid);
+    const docRef = doc(groupsRef, group.uuid);
     return await updateDoc(docRef, { ...group });
 }
 
+export async function resetGroup(groupId: string) {
+    const docRef = doc(groupsRef, groupId);
+    const group = await getGroup(groupId);
+    const resetGroup = { ...group, tasks: resetTaskArray(group.tasks!) };
+    return await updateDoc(docRef, { ...resetGroup });
+}
+
 export async function setGroup(group: Group) {
-    const docRef = doc(GROUPS, group.uuid);
+    const docRef = doc(groupsRef, group.uuid);
     return await setDoc(docRef, group);
 }
 
 export async function deleteGroup(id: string) {
-    const docRef = doc(GROUPS, id);
+    const docRef = doc(groupsRef, id);
     return await deleteDoc(docRef);
+}
+
+export async function deleteTask(task: Task) {
+    let tasks: Task[];
+    if (task.groupId) {
+        const group = await getGroup(task.groupId);
+        tasks = group.tasks;
+        const filteredTasks = filterTaskArray(task.uuid, tasks);
+        return await updateGroup({ ...group, tasks: filteredTasks });
+    } else {
+        tasks = await getInbox();
+        const filteredTasks = filterTaskArray(task.uuid, tasks);
+        return await updateInbox(filteredTasks);
+    }
+}
+
+export async function updateTask(task: Task) {
+    let tasks: Task[];
+    if (task.groupId) {
+        const group = await getGroup(task.groupId);
+        tasks = group.tasks;
+        const updatedTasks = updateArrayTask(task, tasks);
+        return await updateGroup({ ...group, tasks: updatedTasks });
+    } else {
+        tasks = await getInbox();
+        const updatedTasks = updateArrayTask(task, tasks);
+        return await updateInbox(updatedTasks);
+    }
 }
